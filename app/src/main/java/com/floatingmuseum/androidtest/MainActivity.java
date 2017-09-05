80d4e40f12581a3a762191102f181caecdad2991
floatingmuseum <floatingmuseumyan@gmail.com>
2017-09-05 17:21:46 +0800

    2017.9.5

--- app/src/main/java/com/floatingmuseum/androidtest/MainActivity.java
+++ app/src/main/java/com/floatingmuseum/androidtest/MainActivity.java
@@ -2,14 +2,24 @@ package com.floatingmuseum.androidtest;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.MaskFilterSpan;
+import android.text.style.StrikethroughSpan;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.floatingmuseum.androidtest.base.BaseActivity;
 import com.floatingmuseum.androidtest.functions.FunctionsActivity;
@@ -37,6 +47,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     Button btThirdParties;
     @BindView(R.id.activity_main)
     LinearLayout activityMain;
+    @BindView(R.id.tv_test_blur_text)
+    TextView tvTestBlurText;
 
     private String[] needPermissions = {Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -92,6 +104,46 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         Logger.d("差集测试...耗时:" + (System.currentTimeMillis() - startTime));
 
         initPermission(needPermissions);
+
+        testBlurText();
+    }
+
+    private void testBlurText() {
+        MaskFilterSpan maskFilterSpan1 = new MaskFilterSpan(new BlurMaskFilter(20, BlurMaskFilter.Blur.NORMAL));
+        MaskFilterSpan maskFilterSpan2 = new MaskFilterSpan(new BlurMaskFilter(10, BlurMaskFilter.Blur.NORMAL));
+        String text = "spoiler Good movie, not as spectuacular as people have told me but enjoyable /spoiler." +
+                " Even Chris Pine is way better than Gal Gadot." +
+                "Sometimes I got tired of the excessive slowmo fights and so much CGI but the rest is fine.";
+//        String text = "abcdefg hijklmn. opq rst uvw xyz 1234567890 abcdefg hijklmn opq rst uvw xyz 1234567890";
+        SpannableString spanText = new SpannableString(text);
+//        List<Pair<Integer, Integer>> spoilersContainer = new ArrayList<>();
+//        getSpoilerIndex(text,0,spoilersContainer);
+//        for (Pair<Integer, Integer> pair : spoilersContainer) {
+//            spanText.setSpan(maskFilterSpan, pair.first, pair.second, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+//            Logger.d("评论内容...剧透位置:" + pair.toString());
+//        }
+
+//        spanText.setSpan(maskFilterSpan1, 5, 10, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+//        spanText.setSpan(maskFilterSpan2, 15, 20, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
+//        spanText.setSpan(new ForegroundColorSpan(Color.RED),5,10, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+//        spanText.setSpan(new ForegroundColorSpan(Color.RED),15,20, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
+//        spanText.setSpan(new BackgroundColorSpan(Color.BLACK),5,10, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+//        spanText.setSpan(new BackgroundColorSpan(Color.BLACK),15,20, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        spanText.setSpan(new StrikethroughSpan(),5,10, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        spanText.setSpan(new StrikethroughSpan(),15,20, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
+        tvTestBlurText.setText(spanText);
+    }
+
+    private void getSpoilerIndex(String rawComment, int beginIndex, List<Pair<Integer, Integer>> spoilersContainer) {
+        int startIndex = rawComment.indexOf("spoiler", beginIndex);
+        int endIndex = rawComment.indexOf("/spoiler", beginIndex);
+        if (startIndex != -1 && endIndex != -1) {
+            spoilersContainer.add(new Pair<>(startIndex, endIndex));
+            getSpoilerIndex(rawComment, endIndex + "/spoiler".length(), spoilersContainer);
+        }
     }
 
     private void initPermission(String... permissions) {
